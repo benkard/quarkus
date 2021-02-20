02c1e3896e01a736f816a2deee9c2558655149a5
Matthias Andreas Benkard <code@mail.matthias.benkard.de>
2021-02-20 15:20:12 +0100

    MaxParametersTestCase: Refactor and reformat.

--- extensions/undertow/deployment/src/test/java/io/quarkus/undertow/test/MaxParametersTestCase.java
+++ extensions/undertow/deployment/src/test/java/io/quarkus/undertow/test/MaxParametersTestCase.java
@@ -15,6 +15,9 @@ import org.junit.jupiter.api.extension.RegisterExtension;
 import io.quarkus.test.QuarkusUnitTest;
 import io.restassured.RestAssured;
 
+/**
+ * Tests the quarkus.servlet.max-parameters setting.
+ */
 public class MaxParametersTestCase {
 
     @RegisterExtension
@@ -25,29 +28,27 @@ public class MaxParametersTestCase {
 
     @Test
     public void testSmallRequest() {
-        Map<String, String> params = new HashMap<>();
-        for (int i = 0; i < 10; i++) {
-            params.put(String.format("q%d", i), String.format("%d", i));
-        }
-
         RestAssured.given()
-                .params(params)
+                .params(generateParameters(10))
                 .get("/test")
                 .then().statusCode(200).body(Matchers.equalTo("test servlet"));
     }
 
     @Test
     public void testLargeRequest() {
-        Map<String, String> params = new HashMap<>();
-        for (int i = 0; i < 11; i++) {
-            params.put(String.format("q%d", i), String.format("%d", i));
-        }
-
         // At the moment, a throw of io.undertow.util.ParameterLimitException causes a failure
         // to respond to the HTTP request.
         Assertions.assertThrows(SocketTimeoutException.class, () -> RestAssured.given()
-                .params(params)
+                .params(generateParameters(11))
                 .get("/test")
                 .then().statusCode(414));
+    }
+
+    private static Map<String, String> generateParameters(int count) {
+        Map<String, String> params = new HashMap<>();
+        for (int i = 0; i < count; i++) {
+            params.put(String.format("q%d", i), String.format("%d", i));
+        }
+        return params;
     }
 }
